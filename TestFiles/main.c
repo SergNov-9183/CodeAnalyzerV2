7e25acf98f9dca5f782cadbb0c11df4e61d79a27
SergNov-9183 <75534548+SergNov-9183@users.noreply.github.com>
2021-05-12 21:47:32 +0300

    Test files
    added test file and config file

---
+++ TestFiles/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   for (int i = 0; i < 10; ++i) {
+      for (int j = 10; j > 0; --j) {
+printf("%d - %d", i, j);
+      }
+   }
+   int k = 0;
+   while (k < 3) 
+      k =+ 1;
+}
